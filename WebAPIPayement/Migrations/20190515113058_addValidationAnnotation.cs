fa06ab16705d2576195cc4491744aabde8b44b1a
Anonymous <anonymous@localhost.localdomain>
2019-05-15 11:32:19 +0000

    add validation anotation

---
+++ WebAPIPayement/Migrations/20190515113058_addValidationAnnotation.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPIPayement.Migrations
+{
+    public partial class addValidationAnnotation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ExpirationDate",
+                table: "PayementDetails",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CardOwnerName",
+                table: "PayementDetails",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CardNumber",
+                table: "PayementDetails",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CVV",
+                table: "PayementDetails",
+                maxLength: 3,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ExpirationDate",
+                table: "PayementDetails",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CardOwnerName",
+                table: "PayementDetails",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CardNumber",
+                table: "PayementDetails",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CVV",
+                table: "PayementDetails",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 3);
+        }
+    }
+}
--- WebAPIPayement/Models/PayementDetail.cs
+++ WebAPIPayement/Models/PayementDetail.cs
@@ -1,11 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebAPIPayement.Models
 {
     public class PayementDetail
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
+        [Required]
+        [StringLength(10)]
+        [MinLength(3)]
         public string CardOwnerName { get; private set; }
+        [Required]
         public string CardNumber { get; private set; }
+        [Required]
+        [MinLength(10)]
         public string ExpirationDate { get; private set; }
+        [Required]
+        [StringLength(3)]
         public string CVV { get; private set; }
     }
 }
